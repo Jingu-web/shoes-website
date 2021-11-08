c65420ca8823f0bc22be3bd212886a0f15a270e1
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-11-08 21:34:10 +0900

    💫 イメージ画像を上下に移動するアニメーション

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,13 @@
 let slideIndex = 0;
 
 let slider = document.getElementById("slider");
-
 let slides = slider.getElementsByClassName("slide");
-
 let slideControl = document.getElementById("slide-control");
-
 let slideControlItems =
   slideControl.getElementsByClassName("slide-control-item");
 
 slider.style.marginTop = "-" + slideIndex + "00vh";
+
 setTimeout(() => {
   slideControlItems[slideIndex].classList.add("active");
   slides[slideIndex].classList.add("active");
@@ -28,9 +26,11 @@ chooseProduct = (index) => {
   let currSlide = slider.querySelector(".slide.active");
   currSlide.classList.remove("active");
 
-  slider.style.marginTop = "-" + slideIndex + "00vh";
-  slideControlItems[slideIndex].classList.add("active");
-  slides[slideIndex].classList.add("active");
+  setTimeout(() => {
+    slider.style.marginTop = "-" + slideIndex + "00vh";
+    slideControlItems[slideIndex].classList.add("active");
+    slides[slideIndex].classList.add("active");
+  }, 1500);
 };
 
 Array.from(slideControlItems).forEach((e, index) => {
