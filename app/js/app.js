896b3931ac18a64d9371198d3ff42f23edfe763d
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-11-08 17:59:40 +0900

    ✨

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-let slideIndex = 2;
+let slideIndex = 0;
 
 let slider = document.getElementById("slider");
 
@@ -26,9 +26,11 @@ chooseProduct = (index) => {
   currSlideControl.classList.remove("active");
 
   let currSlide = slider.querySelector(".slide.active");
+  currSlide.classList.remove("active");
 
   slider.style.marginTop = "-" + slideIndex + "00vh";
   slideControlItems[slideIndex].classList.add("active");
+  slides[slideIndex].classList.add("active");
 };
 
 Array.from(slideControlItems).forEach((e, index) => {
